b39718623418daec9b0df01c039d7680ea91b46e
Anthony Feenster <afeenster@google.com>
2020-06-25 17:44:39 +0000

    Making the skeleton for the create trip page. Addidng fake inputs and giving IDs to all the elements.

---
+++ frontend/src/main/webapp/view_trip.js
@@ -0,0 +1,39 @@
+
+function fetchAndSetTripPlace() {
+  const eventsList = fetchPlace();
+  eventsList.forEach(createPlacesElement);
+}
+
+function fetchPlace() {
+  var randomEvent = [
+    {'name': 'Random Event', 'description': 'This is the description'},
+    {
+      'name': 'Middle Event',
+      'description': 'Just making sure my function works'
+    },
+    {'name': 'Last Event', 'description': 'Last event just checking'},
+  ];
+
+  return randomEvent;
+}
+
+function createPlacesElement(event) {
+  const tripContainer = document.getElementById('places');
+
+  const nameElement = createEventNameElement(event.name);
+  const descriptionElement = createDescription(event.description);
+
+  tripContainer.appendChild(nameElement);
+  tripContainer.appendChild(descriptionElement);
+}
+
+function createEventNameElement(text) {
+  const dtElement = document.createElement('dt');
+  dtElement.innerText = text;
+  return dtElement;
+}
+function createDescription(text) {
+  const ddElement = document.createElement('dd');
+  ddElement.innerText = '-  ' + text;
+  return ddElement;
+}
