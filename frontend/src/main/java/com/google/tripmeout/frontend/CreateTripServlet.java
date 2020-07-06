867c933e6bbc90686463c5010fd828824eef93dc
Anthony Feenster <afeenster@google.com>
2020-07-06 17:25:46 +0000

    Adding testing for the Create Trips Servlet.

--- frontend/src/main/java/com/google/tripmeout/frontend/CreateTripServlet.java
+++ frontend/src/main/java/com/google/tripmeout/frontend/CreateTripServlet.java
@@ -1,11 +1,14 @@
 package com.google.sps.tripmeout.frontend;
 
 import com.google.tripmeout.frontend.TripModel;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
 
 @WebServlet("/createTrip")
 public final class CreateTripServlet extends HttpServlet {
   private final TripStorage storage;
   private static final String USER_TRIPS_ID_KEY = "userID";
+  private static final String SINGLE_TRIP_ID_KEY = "tripID";
 
 
   @Inject
@@ -15,12 +18,17 @@ public final class CreateTripServlet extends HttpServlet {
     
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    Query query = new Query(USER_TRIPS_ID_KEY);
+    PreparedQuery userID = datastore.prepare(query);
+    
     response.setContentType("application/json;");
-    response.getWriter().print(new Gson().toJson(storage.getAllUserTrips()));
+    response.getWriter().print(new Gson().toJson(storage.getAllUserTrips(userID)));
   }
 
   @Override
   public void doDelete(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    storage.removeTrip()
+    Query query = new Query(SINGLE_TRIP_ID_KEY);
+    PreparedQuery tripId = datastore.prepare(query);
+    storage.removeTrip(tripId)
   }
 }
