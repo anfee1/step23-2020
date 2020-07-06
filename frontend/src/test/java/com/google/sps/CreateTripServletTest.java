4c3a665f37a634a5da8aac60de052a1b33ba3de4
Anthony Feenster <afeenster@google.com>
2020-07-06 17:31:30 +0000

    Adding Errors to the Test file.

--- frontend/src/test/java/com/google/sps/CreateTripServletTest.java
+++ frontend/src/test/java/com/google/sps/CreateTripServletTest.java
@@ -7,6 +7,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import java.util.List;
+import com.google.tripmeout.frontend.storage.InMemoryPlaceVisitStorage;
+import com.google.tripmeout.frontend.PlaceVisitModel;
+import com.google.tripmeout.frontend.error.PlaceVisitNotFoundException;
 
 @RunWith(JUnit4.class)
 public final class InMemoryPlaceVisitStorageTest {
