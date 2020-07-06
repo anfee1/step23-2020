605600f0feaae0b6d31a4cd1f33059a8509084fe
Anthony Feenster <afeenster@google.com>
2020-07-06 22:35:28 +0000

    Making necessary changes to get ready for testing the servlet.

--- frontend/src/main/java/com/google/tripmeout/frontend/TripServlet.java
+++ frontend/src/main/java/com/google/tripmeout/frontend/TripServlet.java
@@ -1,18 +1,26 @@
 package com.google.sps.tripmeout.frontend;
 
 import com.google.tripmeout.frontend.TripModel;
+import com.google.tripmeout.frontend.storage.TripStorage;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
+import com.google.gson.Gson;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import java.io.IOException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/createTrip")
-public final class CreateTripServlet extends HttpServlet {
+@Singleton
+public final class TripServlet extends HttpServlet {
   private final TripStorage storage;
   private static final String USER_TRIPS_ID_KEY = "userID";
   private static final String SINGLE_TRIP_ID_KEY = "tripID";
 
 
   @Inject
-  public CreateTripServlet(TripStorage storage) {
+  public TripServlet(TripStorage storage) {
     this.storage = storage;
   }
     
@@ -29,6 +37,6 @@ public final class CreateTripServlet extends HttpServlet {
   public void doDelete(HttpServletRequest request, HttpServletResponse response) throws IOException {
     Query query = new Query(SINGLE_TRIP_ID_KEY);
     PreparedQuery tripId = datastore.prepare(query);
-    storage.removeTrip(tripId)
+    storage.removeTrip(tripId);
   }
 }
---
+++ frontend/src/test/java/com/google/sps/TripServletTest.java
@@ -0,0 +1,40 @@
+package com.google.sps;
+
+import com.google.tripmeout.frontend.TripServlet;
+import org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+public class MyServletTest {
+    private TripServlet servlet;
+    private MockHttpServletRequest request;
+    private MockHttpServletResponse response;
+
+    @Before
+    public void setUp() {
+        servlet = new CreateTripServlet();
+        request = new MockHttpServletRequest();
+        response = new MockHttpServletResponse();
+    }
+
+    @Test
+    public void correctUsernameInRequest() throws ServletException, IOException {
+        request.addParameter("userID", "12345");
+
+        servlet.doGet(request, response);
+
+        assertEquals("application/json;", response.getContentType());
+
+        // ... etc
+    }
+}
